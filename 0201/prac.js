54e54bdd48edf6a8fcc1ac5f5e30b6d16d656a9b
jwy10 <1998dnjsdud@gmail.com>
2024-02-01 13:53:35 +0900

    0201

---
+++ 0201/prac.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const PORT = 8001;
+
+//body-parser
+app.use(express.urlencoded({extended:true})); //x-www-form-urlencoded방식
+app.use(express.json()) //json 방식
+app.set('view engine', 'ejs');
+app.set('views','./views')
+
+//router
+app.get('/',(req, res)=>{
+  res.render('prac');
+
+});
+
+app.get('/getForm', (req,res) => {
+  //get 방식일 때
+  console.log(req.query)
+  res.render('pracResult',{ title: 'GET요청 결과',userInfo: req.query })
+})
+
+app.post('/postForm', (req,res) => {
+  //post방식일때
+  console.log(req.body);
+  res.render('pracResult',{ title: 'POST요청 결과',userInfo: req.body })
+})
+
+//서버실행
+app.listen(PORT, ()=>{
+  console.log(`http://localhost:${PORT}`);
+})
